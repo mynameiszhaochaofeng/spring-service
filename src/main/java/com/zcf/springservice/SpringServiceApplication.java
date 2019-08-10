93621a700a6cea9d4b7357c053e093691e2f282e
zhaochaofeng <zhaochaofeng@nbeport.com>
2019-08-10 16:32:41 +0800

    初次提交

---
+++ src/main/java/com/zcf/springservice/SpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zcf.springservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceApplication.class, args);
+    }
+
+}
